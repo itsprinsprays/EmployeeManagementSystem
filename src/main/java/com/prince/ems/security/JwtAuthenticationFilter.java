5346f89b0cd561731512f0634e58e87d9b6e43ba
itsprinsprays <prncbntz@gmail.com>
2026-03-07 21:28:59 +0800

    Security Feature : JwthAuthenticationFilter Added

--- src/main/java/com/prince/ems/security/JwtAuthenticationFilter.java
+++ src/main/java/com/prince/ems/security/JwtAuthenticationFilter.java
@@ -2,6 +2,46 @@ package com.prince.ems.security;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+	
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil,
+                                   CustomUserDetailsService customUserDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+	
+	@Override
+	public void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String header = request.getHeader("Authorization");
+		
+		String token = null;
+		String username = null;
+		
+		if(header != null && header.startsWith("Bearer ")) {
+			
+			token = header.substring(7);
+			
+			if(jwtUtil.validateToken(token)) {
+				username = jwtUtil.extractUsername(token);
+			}
+			
+		}
+	}
+	
 
 }
