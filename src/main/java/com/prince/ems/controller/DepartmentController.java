402ecc15d48f6273f118438f6947e8d05523a0e9
itsprinsprays <prncbntz@gmail.com>
2026-03-21 22:16:23 +0800

    Department Feature : Change the getActiveDepartment into getDepartmentStatus

--- src/main/java/com/prince/ems/controller/DepartmentController.java
+++ src/main/java/com/prince/ems/controller/DepartmentController.java
@@ -60,11 +60,12 @@ public class DepartmentController {
 		return ResponseEntity.ok(response);
 	}
 	
-	@GetMapping("/active")
-	public ResponseEntity<Page<DepartmentResponseDTO>> getActiveDepartment(@RequestParam(defaultValue = "0") int page,
-																	 @RequestParam(defaultValue = "5") int size) {
+	@GetMapping("/status/{status}")
+	public ResponseEntity<Page<DepartmentResponseDTO>> getDepartmentStatus(@RequestParam(defaultValue = "0") int page,
+																	 @RequestParam(defaultValue = "5") int size,
+																	 @PathVariable Status status) {
 		Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
-		Page<DepartmentResponseDTO> response = serv.getActiveDepartment(pageable);
+		Page<DepartmentResponseDTO> response = serv.getActiveDepartment(status,pageable);
 		return ResponseEntity.ok(response);
 		
 	}
--- src/main/java/com/prince/ems/service/DepartmentService.java
+++ src/main/java/com/prince/ems/service/DepartmentService.java
@@ -36,12 +36,6 @@ public class DepartmentService {
 	public CreateDepartmentResponseDTO createDepartment(DepartmentRequestDTO dto) {    //Create Department
 		if(repo.existsByName(dto.getName()))
 			throw new DuplicateResponseException(dto.getName() + " Department is already existing");
-//		
-//		if(dto.getName() != null && dto.getName().length() < 5)
-//			throw new BadRequestException("Department name must be at least 5 characters");
-//		else if(dto.getName().length() > 30) 
-//			throw new BadRequestException("Department name must not exceed 30 characters");
-		
 		
 		Department department = new Department();
 		department.setName(dto.getName());
@@ -60,8 +54,8 @@ public class DepartmentService {
 	}
 	
 	@PreAuthorize("hasAnyRole('ADMIN','HR')")
-	public Page<DepartmentResponseDTO> getActiveDepartment(Pageable pageable) {       //Get All Active Departments
-			Page<Department> page = repo.findByStatus(Status.ACTIVE, pageable);
+	public Page<DepartmentResponseDTO> getDepartmentStatus(Status status, Pageable pageable) {       //Get All Active Departments
+			Page<Department> page = repo.findByStatus(status, pageable);
 			return DepartmentMapper.activeDepartmentResponse(page, "ACTIVE DEPARTMENTS");
 	}
 	
