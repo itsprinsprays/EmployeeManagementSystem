8e87443d7ca065cc8683652807172072dfa009d8
itsprinsprays <prncbntz@gmail.com>
2026-03-30 19:55:09 +0800

    Log In Feature: Add Validation on Login

--- src/main/java/com/prince/ems/service/AuthService.java
+++ src/main/java/com/prince/ems/service/AuthService.java
@@ -7,27 +7,44 @@ import org.springframework.stereotype.Service;
 
 import com.prince.ems.dto.login.LoginRequestDTO;
 import com.prince.ems.dto.login.LoginResponseDTO;
+import com.prince.ems.entity.User;
+import com.prince.ems.exception.BadRequestException;
 import com.prince.ems.mapper.LoginMapper;
+import com.prince.ems.repository.UserRepository;
 import com.prince.ems.security.JwtUtil;
 
 @Service
 public class AuthService {
 	
-	@Autowired
+	private UserRepository urepo;
+	
 	private AuthenticationManager authenticationManager;
 	
-	@Autowired
 	private JwtUtil util;
 	
+	@Autowired
+	public AuthService(AuthenticationManager authenticationManager, JwtUtil util, UserRepository urepo) {
+		this.authenticationManager = authenticationManager;
+		this.util = util;
+		this.urepo = urepo;
+	}
+	
 	public LoginResponseDTO login(LoginRequestDTO request) {
+		
+		User user = urepo.findByUsername(request.getUsername())
+			.orElseThrow(() -> new BadRequestException("User not found")
+					  );
+		
+		if(request.getPassword() != user.getPassword())
+			throw new BadRequestException("Wrong Password");
+
 		
 		authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(
 						request.getUsername(),
 						request.getPassword()
-						)
-				);
-		
+					 ));
+			
 		String token = util.generateToken(request.getUsername());
 		
 		return LoginMapper.tokenResponse(token);
