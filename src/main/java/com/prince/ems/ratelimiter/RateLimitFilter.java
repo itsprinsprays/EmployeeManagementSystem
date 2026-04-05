1528ce7149ac0dc71667636cc5f4ee9e7a19c0eb
itsprinsprays <prncbntz@gmail.com>
2026-04-05 16:53:06 +0800

    Rate Limiting Feature : Add TTL and fix the throttle key

--- src/main/java/com/prince/ems/ratelimiter/RateLimitFilter.java
+++ src/main/java/com/prince/ems/ratelimiter/RateLimitFilter.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.prince.ems.security.JwtUtil;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,16 +16,33 @@ import jakarta.servlet.http.HttpServletResponse;
 public class RateLimitFilter extends OncePerRequestFilter{
 	
 	private final TokenBucketService tokenBucketService;
+	private final JwtUtil jwtUtil;
 	
-	public RateLimitFilter(TokenBucketService tokenBucketService) {
+	public RateLimitFilter(TokenBucketService tokenBucketService, JwtUtil jwtUtil) {
 		this.tokenBucketService = tokenBucketService;
+		this.jwtUtil = jwtUtil;
 	}
 	
 	@Override
 	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
 													throws ServletException, IOException {
 		
-		String ip = request.getRemoteAddr(); //replace with userID
+		String header = request.getHeader("Authorization");
+		
+		String token = null;
+		String username = null;
+		
+		if(header != null && header.startsWith("Bearer")) {
+			token = header.substring(7);
+			
+			username = jwtUtil.extractUsername(token);
+		}
+		
+		String ip = username; //replace with userID
+		
+		if(ip == null) {
+			ip = request.getRemoteAddr();
+		}
 		
 		if(!tokenBucketService.allowRequest(ip)) {
 			response.setStatus(429);
@@ -31,7 +50,7 @@ public class RateLimitFilter extends OncePerRequestFilter{
 			return;
 		}
 		
-		filterChain.doFilter(request, response);
+		filterChain.doFilter(request, response);  
 		
 	}
 		
--- src/main/java/com/prince/ems/ratelimiter/TokenBucketService.java
+++ src/main/java/com/prince/ems/ratelimiter/TokenBucketService.java
@@ -1,5 +1,7 @@
 package com.prince.ems.ratelimiter;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +10,17 @@ public class TokenBucketService {
 	
 	private final RedisTemplate<String, Object> redisTemplate;
 	
-	private static final int maxToken = 5;
-	private static final int refillRate = 0; //per second
+	private static final int maxToken = 10;
+	private static final int refillRate = 1; //per second
 	
 	public TokenBucketService(RedisTemplate<String, Object> redisTemplate) {
 		this.redisTemplate = redisTemplate;
 	}
 	
 	public boolean allowRequest(String key) {
-		
+	
+		 
+		//throttle key stored in the redis-cli
 		String redisKey = "bucket:" + key;
 		
 		TokenBucket bucket = (TokenBucket) redisTemplate.opsForValue().get(redisKey);
@@ -26,12 +30,13 @@ public class TokenBucketService {
 		//First Request
 		if(bucket == null) {
 			bucket = new TokenBucket(maxToken - 1, now);
-			redisTemplate.opsForValue().set(redisKey, bucket);
-			return true;
+			redisTemplate.opsForValue().set(redisKey, bucket, 1, TimeUnit.MINUTES);
+			return true; 
 		}
 		
 		//Refill Token
 		long secondsPassed = (now - bucket.getLastRefillTime()) / 1000;
+//		long intervalPassed = secondsPassed / 15;
 		int tokensToAdd = (int) (secondsPassed * refillRate);
 		
 		if(tokensToAdd > 0) {
@@ -43,7 +48,7 @@ public class TokenBucketService {
 		//Consume Token
 		if(bucket.getToken() > 0) {
 			bucket.setToken(bucket.getToken() - 1);
-			redisTemplate.opsForValue().set(redisKey, bucket);
+			redisTemplate.opsForValue().set(redisKey, bucket, 1, TimeUnit.HOURS);
 			return true;
 		}
 		return false;
